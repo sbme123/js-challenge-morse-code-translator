50a0cebe30e55a037df0e72447b74488f8b0f3e9
sbme23 <93839730+sbme23@users.noreply.github.com>
2022-02-12 16:26:42 +0000

    Linked input and output
    Linked input and output boxed to the js file

--- morse-code-translator.js
+++ morse-code-translator.js
@@ -8,3 +8,21 @@ able to translate based on user input from the DOM.*/
 4. ** extension – Should receive inputs from the HTML
 */
 
+// Create a class for the translator 
+// create a class to link the dom ? 
+// 
+
+// Need to link the text area boxes from the html input and output
+
+// Possible object / array to hold all the morese and alphabet. 
+
+// Could you use a switch case to work out the translation 
+// POtentially the switch case could hold both the morse and aplhabet together. 
+
+/* STARTING THE MORSE CODE TRANSLATER */
+
+const input = document.querySelector(".input")
+// input.value = "Hello"
+
+const output = document.querySelector(".output")
+output.innerHTML = "Test"
